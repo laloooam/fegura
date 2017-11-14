32e6c6ab3297ed21ce7b4fac00718922faa36bf4
infectus <infectus@DESKTOP-B67U2GK>
2017-11-15 00:51:53 +0100

    se corrigieron errores , se cambiaron nombres de variables , se añadio dos if a la clase paralelogramo

--- src/main/Paralelogramo.java
+++ src/main/Paralelogramo.java
@@ -13,17 +13,17 @@ public class Paralelogramo extends Triangulo{
     Scanner scan = new Scanner(System.in);
     
     public void ingresarParalelogramo(){
-        System.out.println("ingrese cordanada 1:");
+        System.out.println("Ingrese Coordenada 1:");
         String separador1 = scan.nextLine();
         String[] cadena1 = separador1.split(",");
         String a1 = cadena1[0];
         String a2 = cadena1[1];
-        double asd1 = Double.parseDouble(a1);
-        double asd2 = Double.parseDouble(a2);
-        a [0] = asd1;
-        a [1] = asd2;
+        double punto1 = Double.parseDouble(a1);
+        double punto2 = Double.parseDouble(a2);
+        a [0] = punto1;
+        a [1] = punto2;
         
-        System.out.println("ingrese cordenada 2:");
+        System.out.println("Ingrese Coordenada 2:");
         String separador2 = scan.nextLine();
         String[] cadena2 = separador2.split(",");
         String a3 = cadena2[0];
@@ -31,7 +31,7 @@ public class Paralelogramo extends Triangulo{
         b [0] = Double.parseDouble(a3);
         b [1] = Double.parseDouble(a4);
 
-        System.out.println("ingrese cordenada 3:");
+        System.out.println("Ingrese Coordenada 3:");
         String separador3 = scan.nextLine();
         String[] cadena3 = separador3.split(",");
         String a5 = cadena3[0];
@@ -39,7 +39,7 @@ public class Paralelogramo extends Triangulo{
         c [0] = Double.parseDouble(a5);
         c [1] = Double.parseDouble(a6);
         
-        System.out.println("ingrese cordenada 3:");
+        System.out.println("Ingrese Coordenada 4:");
         String separador4 = scan.nextLine();
         String[] cadena4 = separador4.split(",");
         String a7 = cadena3[0];
@@ -53,8 +53,16 @@ public class Paralelogramo extends Triangulo{
         double distancia4 = Math.sqrt(Math.pow(d[0]-d[1],2)+Math.pow(a[0]-a[1],2));
         
         if(distancia1 == distancia3 && distancia2 == distancia4 ){
-            System.out.println("cuadrado");
+            System.err.println("[Cuadrado]");
         }
+        if(distancia1 !=distancia2 && distancia4!= distancia3){
+            System.err.println("[Rectangulo]");
+        }
+        if(distancia1 >= distancia2 && distancia3 >= distancia4){
+            System.err.println("[Rombo]");
+        }
+        
+        
         
     }
 
